bf7381746dbf537f500f9db9100cd1438af10d22
dvash999 <myselfandme@gmail.com>
2018-12-05 21:10:20 +0200

    mongo

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '331333517969-uflkqt05pnuu90uj5mt0rhbf1vaj3btk.apps.googleusercontent.com',
+  googleClientSecret: 'HB9QcyqRiTtkucT-S-Zw_kZn',
+  mongoDB_URI: 'mongodb://dash99:Vhv1sho2973@ds119663.mlab.com:19663/feedbackapp',
+};
