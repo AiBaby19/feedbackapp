655930305e3d7e5208891c19fd06934e113a8394
dvash999 <myselfandme@gmail.com>
2018-12-18 19:06:03 +0200

    updating for herokuapp

--- server/index.js
+++ server/index.js
@@ -28,6 +28,10 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE === 'production') {
   app.use(express.static('client/buid'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 
