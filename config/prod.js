b0f18ef563b2fefdf7da29e266bacddae99b705e
dvash999 <myselfandme@gmail.com>
2019-01-05 18:50:45 +0200

    master fixCLean

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: process.env.USER,
+  pass: process.env.PASS,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
+};
