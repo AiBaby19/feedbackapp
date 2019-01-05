9765fbaf8c335a3fb0b8309a7746192f5c1a13b3
dvash999 <myselfandme@gmail.com>
2019-01-05 14:21:56 +0200

    finished showing all surveys

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -6,9 +6,36 @@ class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div style={{marginBottom: "25px"}}>
+          <div className="card draken-1" style={{margin: "0 auto", marginTop: '45px', width: '80%'}}key={survey._id}>
+            <div className="card-content">
+              <span className="card-title">{survey.title}</span>
+              <p>
+                {survey.body}
+              </p>
+              <p className="right">
+                Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+              </p>
+            </div>
+            <div className="card-action">
+              <a>Yes: {survey.yes}</a>
+              <a>No: {survey.no}</a>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
     render() {
         return (
-            <div>SurveyList</div>
+            <div>
+              <h3>All Sent Surveys</h3>
+              {this.renderSurveys()}
+            </div>
         )
     }
 }
